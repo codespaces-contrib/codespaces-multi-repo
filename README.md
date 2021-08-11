e84b2231ec0d397876fddd08007c6ccdc2d152e7
Chuck Lantz <clantz@microsoft.com>
2021-08-11 14:56:37 -0700

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# codespaces-multi-repo
+# Multi-Repo Codespaces Example
+
+Generally when you are dealing with a multi-repository application, you end up with a primary "bootstrap" repository that contaains getting started information and local configuraiton files.
+
+GitHub Codespaces conceptually fits well into this model since you could create a Codespace from this boostrap and then clone other repositories into it. However, to improve security, Codespaces currelty provides a repository scoped token by default. However, you can use SSH keys or a personal access token instead. This sample illustrates how to configure a user secret and assign it to a Codespace so that it is used instead of the default `GITHUB_TOKEN`.
+
+
+
---
+++ clone-repos.sh
@@ -0,0 +1,27 @@
+#!/usr/env/bin bash
+script_folder="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+workspaces_folder="$(cd "${script_folder}/.." && pwd)"
+
+clone-repo()
+{
+    cd "${workspaces_folder}"
+    if [ ! -d "$1" ]; then
+        git clone "https://github.com/$1"
+    else 
+        echo "Already cloned $1"
+    fi
+}
+
+if [ "${CODESPACES}" = "true" ]; then
+    # Remove the default credential helper
+    sudo sed -i -E 's/helper =.*//' /etc/gitconfig
+
+    # Add one that just uses secrets available in the Codespace
+    git config --global credential.helper '!f() { sleep 1; echo "username=${GH_USER}"; echo "password=${GH_TOKEN}"; }; f'
+fi
+
+if [ -f "repos-to-clone" ]; then
+    while IFS= read -r repository; do
+        clone-repo "$repository"
+    done < "${script_folder}/repos-to-clone.list"
+fi
